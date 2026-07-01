86e92539caea8d19f8c1beb07f286aeec64f5f7e
Kamidzu9 <senvey228@gmail.com>
2026-07-01 12:50:35 +0200

    task 3

--- typescript/week-02-functions/task-03-is-positive/solutions.ts
+++ typescript/week-02-functions/task-03-is-positive/solutions.ts
@@ -1,4 +1,11 @@
 export function isPositive(num: number): boolean {
   // TODO: Gib true zurück wenn num > 0, sonst false
-  return false;
+  if ( num > 0 ) 
+    {
+      return true;
+    }
+  else
+    {
+      return false;
+    }
 }
